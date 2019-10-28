fa50fec186654ffa4ddbf09e06d3b543ab435382
Francesco <franc.ns@gmail.com>
2019-10-28 14:49:07 +0100

    added js in the 'palindroma' exercise

--- palindroma/assets/js/script.js
+++ palindroma/assets/js/script.js
@@ -1,1 +1,23 @@
 // Funzione per capire se la parola è palindroma
+
+// variabili
+var userWord, output;
+
+// inserimento parola da utente
+userWord = prompt('Inserisci una parola');
+
+// function
+output = palindrome(userWord);
+
+// output
+console.log(output);
+
+// dichiarazione function
+function palindrome (word) {
+  var reverseWord = word.split("").reverse().join("");
+  if (word === reverseWord) {
+    return 'La parola è palindroma';
+  } else {
+    return 'La parola non è palindroma';
+  }
+}
